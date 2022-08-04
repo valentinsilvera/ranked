969be894ba83cb11fac62a086fb54a046e88500c
Valentin <valentin.silvera@code.berlin>
2022-08-04 17:40:48 +0200

    app created

---
+++ ranked/ranked/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ranked
+//
+//  Created by Valentin Silvera on 04.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ranked/ranked/RankedApp.swift
@@ -0,0 +1,17 @@
+//
+//  rankedApp.swift
+//  ranked
+//
+//  Created by Valentin Silvera on 04.08.22.
+//
+
+import SwiftUI
+
+@main
+struct rankedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
